54f97b0752acf1ccaca690c619583eb0542a1459
delly0 <adelefinney03@gmail.com>
2025-06-15 14:13:15 +1000

    changed to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import GlowCursor from "./components/GlowCursor";
